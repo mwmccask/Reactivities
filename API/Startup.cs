66164e27a50c6b00a90488318125733ee6c89830
Matthew McCaskill <matthew@mydrugcosts.com>
2020-05-18 22:34:30 -0400

    Initial commit of React app

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,10 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy => policy
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:3000")));
             services.AddControllers();
         }
 
@@ -45,6 +49,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
