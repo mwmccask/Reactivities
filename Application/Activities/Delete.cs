5d8373b0e7dc32afe2fdbf0e117189b4b97a3f58
Matthew McCaskill <matthew@mydrugcosts.com>
2020-05-19 21:00:07 -0400

    Added handlers and MediatR

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,41 @@
+using MediatR;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext context;
+
+            public Handler(DataContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await this.context.Activities.FindAsync(new object[] { request.Id }, cancellationToken);
+
+                if (activity == null)
+                    throw new Exception("Could not find activity");
+
+                this.context.Activities.Remove(activity);
+                var success = await this.context.SaveChangesAsync(cancellationToken) > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
---
+++ Application/Activities/Details.cs
@@ -0,0 +1,34 @@
+using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Details
+    {
+        public class Query : IRequest<Activity>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Activity>
+        {
+            private readonly DataContext context;
+
+            public Handler(DataContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activity = await this.context.Activities.FindAsync(new object[] { request.Id }, cancellationToken);
+
+                return activity;
+            }
+        }
+    }
+}
